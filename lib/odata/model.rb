f3cb36d8c70e16bcf6b1af4b92b0ba6f65dad0bb
James Thompson <james@buypd.com>
2014-07-07 17:13:08 -0600

    Fixed typo in documentation. [ci skip]

--- lib/odata/model.rb
+++ lib/odata/model.rb
@@ -3,8 +3,8 @@ require 'odata/model/version'
 # OData is the parent namespace for the OData::Model project.
 module OData
   # OData::Model provides a way to map from OData::Entity instances, as
-  # returned by the OData gem to Ruby objects that will work with Rails via the
-  # ActiveModel semantics.
+  # returned by the OData gem, to Ruby objects that will work with Rails via
+  # the ActiveModel semantics.
   module Model
     # Your code goes here...
   end
