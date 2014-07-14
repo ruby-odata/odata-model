a493d9e1b4dbe101666b1689ed6d15c8ab6a96d6
James Thompson <james@buypd.com>
2014-07-14 15:21:22 -0600

    Implelented #skip and #limit for OData::Query::Builder.

--- lib/odata/query/builder.rb
+++ lib/odata/query/builder.rb
@@ -7,7 +7,32 @@ module OData
         @entity_set = entity_set
       end
 
+      # Set the number of entities to skip for the final query.
+      # @param value [to_i,nil]
+      # @return [self]
+      def skip(value)
+        value = nil if value == 0
+        query_structure[:skip] = value.try(:to_i)
+        self
+      end
+
+      # Set the number of entities to return in the final query.
+      # @param value [to_i,nil]
+      # @return [self]
+      def limit(value)
+        value = nil if value == 0
+        query_structure[:top] = value.try(:to_i)
+        self
+      end
+
       private
+
+      def query_structure
+        @query_structure ||= {
+            top:   nil,
+            skip:  nil
+        }
+      end
 
       def entity_set
         @entity_set
--- spec/query/builder/pagination_spec.rb
+++ spec/query/builder/pagination_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe OData::Query::Builder do
+  let(:subject) { OData::Query::Builder.new(Product.odata_entity_set) }
+
   describe 'pagination method' do
     describe '#skip' do
-      it { pending; fail }
+      it { expect(subject).to respond_to(:skip) }
+      it { expect(subject.skip(5)).to be_a(OData::Query::Builder) }
+      it { expect(subject.skip(5)).to eq(subject) }
     end
 
     describe '#limit' do
-      it { pending; fail }
+      it { expect(subject).to respond_to(:limit) }
+      it { expect(subject.limit(5)).to be_a(OData::Query::Builder) }
+      it { expect(subject.limit(5)).to eq(subject) }
     end
   end
 end
