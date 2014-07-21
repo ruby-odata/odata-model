7a714336cbd238d178697602f4f36b8bbc29d9e0
James Thompson <james@buypd.com>
2014-07-21 14:26:57 -0600

    Implemented OData::Model::QueryProxy#is.

--- lib/odata/model/query_proxy.rb
+++ lib/odata/model/query_proxy.rb
@@ -1,18 +1,37 @@
 module OData
   module Model
     # Provides the proxy between OData::Query and OData::Model.
-    # @api private
     class QueryProxy
+      attr_reader :last_criteria
+
       # Instantiates a new QueryProxy for the supplied OData::Model class.
       # @param model_class [Class]
+      # @api private
       def initialize(model_class)
         @target = model_class
         @query = target.odata_entity_set.query
+        @last_criteria = nil
       end
 
       # Sets up a new criteria for filters for the given property name.
       # @param property_name [Symbol]
+      # @return [self]
+      # @api private
       def where(property_name)
+        @last_criteria = query[target.property_map[property_name]]
+        self
+      end
+
+      # Sets up last criteria with supplied argument sets.
+      # @param arguments [Hash]
+      # @return [self]
+      def is(arguments)
+        raise ArgumentError 'can only accept Hash argument' unless arguments.is_a?(Hash)
+        property_name = last_criteria.property
+        arguments.each do |operator, value|
+          @last_criteria = query[property_name].send(operator.to_sym, value)
+          query.where(@last_criteria)
+        end
         self
       end
 
--- spec/model/query_proxy_spec.rb
+++ spec/model/query_proxy_spec.rb
@@ -1,11 +1,35 @@
 require 'spec_helper'
 
 describe OData::Model::QueryProxy do
-  let(:subject) { OData::Model::QueryProxy.new(Product) }
+  let(:query_proxy) { OData::Model::QueryProxy.new(Product) }
+  let(:subject) { query_proxy }
 
   it { expect(subject).to respond_to(:where) }
   describe '#where' do
-    it { pending; fail }
+    let(:subject) { query_proxy.where(:name) }
+
+    it { expect(subject).to be_a(OData::Model::QueryProxy) }
+    it 'sets up last criteria properly' do
+      expect(subject.last_criteria).to be_a(OData::Query::Criteria)
+      expect(subject.last_criteria.property).to eq(Product.property_map[:name])
+    end
+  end
+
+  it { expect(subject).to respond_to(:is) }
+  describe '#is' do
+    %w{eq ne gt lt ge le}.each do |operator|
+      describe "with :#{operator} operator" do
+        let(:subject) { query_proxy.where(:rating).is(operator.to_sym => 4) }
+
+        it { expect(subject).to be_a(OData::Model::QueryProxy) }
+        it 'sets up last criteria properly' do
+          expect(subject.last_criteria).to be_a(OData::Query::Criteria)
+          expect(subject.last_criteria.property).to eq(Product.property_map[:rating])
+          expect(subject.last_criteria.operator).to eq(operator.to_sym)
+          expect(subject.last_criteria.value).to eq(4)
+        end
+      end
+    end
   end
 
   it { pending; expect(subject).to respond_to(:limit) }
