03b8f55ca31d92891ade97ea2a19a0afc359c272
James Thompson <james@buypd.com>
2014-07-21 14:48:59 -0600

    Added #limit, #skip and #order_by methods to OData::Model::Query::ClassMethods.

--- lib/odata/model/query.rb
+++ lib/odata/model/query.rb
@@ -18,6 +18,30 @@ module OData
           query_proxy.where(property_name.to_sym)
         end
 
+        # Starts a query chain with a limit to entities returned.
+        # @param value [to_i]
+        # @return [OData::Model::Query]
+        def limit(value)
+          query_proxy = OData::Model::QueryProxy.new(self)
+          query_proxy.limit(value.to_i)
+        end
+
+        # Starts a query chain with the number of entities to skip.
+        # @param value [to_i]
+        # @return [OData::Model::Query]
+        def skip(value)
+          query_proxy = OData::Model::QueryProxy.new(self)
+          query_proxy.skip(value.to_i)
+        end
+
+        # Starts a query chain with a order operator for a given property's name.
+        # @param property_name [to_sym]
+        # @return [OData::Model::Query]
+        def order_by(property_name)
+          query_proxy = OData::Model::QueryProxy.new(self)
+          query_proxy.order_by(property_name.to_sym)
+        end
+
         # Enables lookup of model entities by their primary key.
         # @param primary_key_value [to_s] primary key value to lookup
         # @return [OData::Model,nil]
--- spec/model/query_interface_spec.rb
+++ spec/model/query_interface_spec.rb
@@ -13,5 +13,20 @@ describe OData::Model do
     describe 'Product#where' do
       it { expect(Product.where(:name)).to be_a(OData::Model::QueryProxy) }
     end
+
+    it { expect(Product).to respond_to(:limit) }
+    describe 'Product#limit' do
+      it { expect(Product.limit(3)).to be_a(OData::Model::QueryProxy) }
+    end
+
+    it { expect(Product).to respond_to(:skip) }
+    describe 'Product#skip' do
+      it { expect(Product.skip(6)).to be_a(OData::Model::QueryProxy) }
+    end
+
+    it { expect(Product).to respond_to(:order_by) }
+    describe 'Product#order_by' do
+      it { expect(Product.order_by(:rating)).to be_a(OData::Model::QueryProxy) }
+    end
   end
 end
